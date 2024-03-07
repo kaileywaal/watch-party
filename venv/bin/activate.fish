0a2ff9f33b49735d89a7f412e2aaf51b71ecc6a2
Kailey <kaileynwaal@gmail.com>
2024-03-07 12:40:31 -0700

    heroku setup

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/kaileywaal/my_flask_app/venv"
+set -gx VIRTUAL_ENV "/Users/kaileywaal/Desktop/watch-party/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
