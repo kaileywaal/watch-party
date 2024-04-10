a580f6e525c7c994a37da47eba4ab75cbd400088
Kailey <kaileynwaal@gmail.com>
2024-04-10 17:46:34 -0600

    get weather data by location user interface WIP

--- src/app.py
+++ src/app.py
@@ -11,16 +11,17 @@ def main():
     locations_file_path = os.path.join("src", "data", "locations.json")
     with open(locations_file_path, "r") as f:
         locations = json.load(f)
+
     return render_template("index.html", locations=locations)
 
     # if request.method == "GET":
     #     return analyze_weather_data_for_location("Superior, CO")
 
 
-@app.route("/submit_location")
+@app.route("/get-weather-data", methods=["POST"])
 def get_data_for_location():
-    print(request.args)
-    return request.args
+    location = request.form["location"]
+    return location
 
 
 if __name__ == "__main__":
