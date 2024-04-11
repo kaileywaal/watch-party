40f7f04ab13223ab6806e542b6b2caa4c92012b7
Kailey <kaileynwaal@gmail.com>
2024-04-11 13:01:07 -0600

    frontend triggers data collection for location

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, render_template
-from data_analyzer import analyze_weather_data_for_location
 import json
 import os
 from components.LocationDataGateway import LocationDataGateway
 from components.WeatherDataGateway import WeatherDataGateway
+from data_analyzer import analyze_weather_data
+from data_collector import collect_weather_data_for_location
 from dotenv import load_dotenv
 import os
 
@@ -16,24 +17,16 @@ weather_gateway = WeatherDataGateway(os.getenv("DB_PATH"))
 
 @app.route("/")
 def main():
-
-    # locations_file_path = os.path.join("src", "data", "locations.json")
-    # with open(locations_file_path, "r") as f:
-    #     locations = json.load(f)
-
-    print(location_gateway.get_location_list())
-
     return render_template("index.html", locations=location_gateway.get_location_list())
-
-    # if request.method == "GET":
-    #     return analyze_weather_data_for_location("Superior, CO")
 
 
 @app.route("/get-weather-data", methods=["POST"])
 def get_data_for_location():
     location_id = request.form["location"]
+    collect_weather_data_for_location(location_id)
+
     weather = weather_gateway.get_weather_data_by_location(location_id)
-    return weather
+    return analyze_weather_data(weather)
 
 
 if __name__ == "__main__":
--- src/data_collector.py
+++ src/data_collector.py
@@ -1,7 +1,7 @@
 import requests
 from sqlalchemy import create_engine, Column, Integer, String, Numeric, ForeignKey
 from sqlalchemy.orm import declarative_base, sessionmaker
-from components.LocationDataGateway import LocationDataGateway
+from components.LocationDataGateway import LocationDataGateway, Location
 from components.WeatherDataGateway import WeatherDataGateway
 from dotenv import load_dotenv
 import os
@@ -12,13 +12,13 @@ load_dotenv()
 # API constants
 SUPERIOR_CO_LATITUDE = 52.52
 SUPERIOR_CO_LONGITUDE = 13.41
-API_URL = (
-    "https://archive-api.open-meteo.com/v1/archive?latitude="
-    + str(SUPERIOR_CO_LATITUDE)
-    + "&longitude="
-    + str(SUPERIOR_CO_LONGITUDE)
-    + "&start_date=2024-01-01&end_date=2024-04-08&daily=sunshine_duration,daylight_duration&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch"
-)
+# API_URL = (
+#     "https://archive-api.open-meteo.com/v1/archive?latitude="
+#     + str(SUPERIOR_CO_LATITUDE)
+#     + "&longitude="
+#     + str(SUPERIOR_CO_LONGITUDE)
+#     + "&start_date=2024-01-01&end_date=2024-04-08&daily=sunshine_duration,daylight_duration&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch"
+# )
 
 
 location_gateway = LocationDataGateway(os.getenv("DB_PATH"))
@@ -49,20 +49,23 @@ weather_gateway = WeatherDataGateway(os.getenv("DB_PATH"))
 #         return None
 
 
-def collect_weather_data():
+def collect_weather_data_for_location(location_id):
     """Collect weather data from the API and store it in the database"""
-    print("collecting data")
+
+    location: Location = location_gateway.get_location_by_id(location_id)
+    print("collecting data for " + location["location_name"])
+
+    API_URL = (
+        "https://archive-api.open-meteo.com/v1/archive?latitude="
+        + str(location["latitude"])
+        + "&longitude="
+        + str(location["longitude"])
+        + "&start_date=2024-01-01&end_date=2024-04-08&daily=sunshine_duration,daylight_duration&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch"
+    )
+
     response = requests.get(API_URL).json()
 
     result_count = len(response["daily"]["time"])
-    location_id = location_gateway.get_location_id_by_coordinates(
-        response["latitude"], response["longitude"]
-    )
-
-    if not location_id:
-        location_id = location_gateway.add_location(
-            "Superior, CO", response["latitude"], response["longitude"]
-        )
 
     for result in range(result_count):
         date = response["daily"]["time"][result]
@@ -74,22 +77,7 @@ def collect_weather_data():
         )
 
 
-# def get_weather_data():
-#     """Retrieve all weather data from the database"""
-#     weather = session.query(Weather).all()
-#     return [
-#         {
-#             "id": item.id,
-#             "location_id": item.location_id,
-#             "date": item.date,
-#             "sunshine_duration": item.sunshine_duration,
-#             "daylight_duration": item.daylight_duration,
-#         }
-#         for item in weather
-#     ]
-
-
 if __name__ == "__main__":
-    collect_weather_data()  # Collect weather data when the script is run
+    print("Collecting Data")
     print(location_gateway.get_location_list())
     print(weather_gateway.get_all_weather_data())
